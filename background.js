c037da3a586f3d18812e54520938d675f9ee1c3a
Quy, Nguyen Huu (Mr.) <nguyenquy.1096@gmail.com>
2023-09-26 21:15:55 +0700

    add

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let targetUrl = 'https://blogs.nhquydev.net/?utm_source=chrome&utm_medium=extension&utm_campaign=block-opening-tabs',
-    blacklist = ['.*hhpanda\.tv.*'];
+    blacklist = ['.*hhpanda\.tv.*', '.*hh3dhay\.com.*'];
 
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ 'blacklist': blacklist }, function () {
@@ -35,7 +35,7 @@ chrome.tabs.onCreated.addListener(function (tab) {
             let isBlocked = false;
             result.blacklist.forEach(pattern => {
                 let regex = new RegExp(pattern);
-                if (regex.test(previousTab.url)) {
+                if (regex.test(previousTab?.url)) {
                     isBlocked = true;
                     // Remove the tab
                     // chrome.tabs.remove(tab.id);
@@ -43,7 +43,7 @@ chrome.tabs.onCreated.addListener(function (tab) {
                     return;
                 }
             });
-            console.log(`previousTab: ${isBlocked} -> previousTab.url: ${previousTab.url}`); // true or false
+            console.log(`previousTab: ${isBlocked} -> previousTab.url: ${previousTab?.url}`); // true or false
         });
     });
 
@@ -52,7 +52,7 @@ chrome.tabs.onCreated.addListener(function (tab) {
         let isBlocked = false;
         result.blacklist.forEach(pattern => {
             let regex = new RegExp(pattern);
-            if (regex.test(tab.pendingUrl) || regex.test(tab.url)) {
+            if (regex.test(tab?.pendingUrl) || regex.test(tab?.url)) {
                 isBlocked = true;
                 // Remove the tab
                 // chrome.tabs.remove(tab.id);
@@ -60,12 +60,12 @@ chrome.tabs.onCreated.addListener(function (tab) {
                 return;
             }
         });
-        console.log(`current: ${isBlocked} -> tab.pendingUrl: ${tab.pendingUrl} -> tab.url: ${tab.url}`); // true or false
+        console.log(`current: ${isBlocked} -> tab.pendingUrl: ${tab?.pendingUrl} -> tab.url: ${tab?.url}`); // true or false
     });
 });
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (!tabs[0].url.startsWith('chrome://')) {
+    if (!tabs[0]?.url.startsWith('chrome://')) {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id, allFrames: true },
             func: function () {
